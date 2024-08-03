18a486cde260ae4732ba2fe58714dd6216f878ad
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-08-03 16:36:31 -0300

    feat: MainNav component and apply header component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,16 @@
+import { MainNav } from './main-nav';
+
 export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="w-full md:max-w-3xl md:my-0 md:mx-auto flex items-center justify-between">
+      <div>
+        <h1 className="text-base font-nunito font-bold">Lucas Anselmo</h1>
+        <p className="text-sm font-nunito font-medium">
+          Front-end Software Developer
+        </p>
+      </div>
+
+      <MainNav />
     </header>
   );
 }
---
+++ src/components/main-nav.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+
+interface NavLink {
+  to: string;
+  label: string;
+}
+
+const navLinks: NavLink[] = [
+  {
+    to: '',
+    label: 'Home',
+  },
+  {
+    to: '/about',
+    label: 'About',
+  },
+  {
+    to: '/projects',
+    label: 'Projects',
+  },
+  {
+    to: '/articles',
+    label: 'Articles',
+  },
+];
+
+export function MainNav() {
+  return (
+    <nav>
+      <ul className="flex items-center gap-1">
+        {navLinks.map((params, index) => (
+          <li key={index}>
+            <Link
+              to={params.to}
+              className="text-base font-nunito font-light underline decoration-sky-700 decoration-1 underline-offset-1"
+            >
+              {params.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
