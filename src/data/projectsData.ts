2b7a9bcd52826ebec7623f04c8b4eed48ba557ad
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-08-11 21:14:08 -0300

    feat: project data project
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -3,22 +3,132 @@ import { IProjectsData } from '../utils/IProject';
 export const projectsData: IProjectsData[] = [
   {
     title: 'John Text List',
-    technologies: ['React.JS', 'TypeScript', 'TailwindCSS'],
+    technologies: ['React.JS', 'TypeScript', 'ShadcnUI'],
     image: {
-      src: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'Example image',
+      src: 'https://live.staticflickr.com/65535/53917583065_fb5c68ceb5_o.png',
+      alt: 'GitHub Logo',
     },
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto voluptatum sunt enim aspernatur totam fuga, voluptates labore reiciendis nesciunt velit commodi culpa repudiandae, facilis distinctio incidunt sequi omnis. Nulla, dolorem.',
+      'This is a to-do list web application, responsible for helping you organize your routine of tasks that need to be done during the day.',
     links: [
       {
         label: 'View code',
-        url: '#',
+        url: 'https://github.com/LucasAnselmoSilva12345/John-Task-List',
         iconClassName: 'text-sky-700',
       },
       {
         label: 'View website',
-        url: '#',
+        url: 'https://john-task-list.pages.dev/',
+        iconClassName: 'text-sky-700',
+      },
+    ],
+  },
+  {
+    title: 'Rest Countries In The World',
+    technologies: ['React.JS', 'TypeScript', 'TailwindCSS'],
+    image: {
+      src: 'https://live.staticflickr.com/65535/53917583065_fb5c68ceb5_o.png',
+      alt: 'GitHub Logo',
+    },
+    description:
+      'The goal of this project is to showcase all countries worldwide through individual cards displaying their respective flag, name, capital, total population, and continent.',
+    links: [
+      {
+        label: 'View code',
+        url: 'https://github.com/LucasAnselmoSilva12345/restCountriesInTheWorld',
+        iconClassName: 'text-sky-700',
+      },
+      {
+        label: 'View website',
+        url: 'https://restcountriesintheworld.pages.dev/',
+        iconClassName: 'text-sky-700',
+      },
+    ],
+  },
+  {
+    title: 'Hero Hub',
+    technologies: ['React.JS', 'TypeScript', 'TailwindCSS'],
+    image: {
+      src: 'https://live.staticflickr.com/65535/53917583065_fb5c68ceb5_o.png',
+      alt: 'GitHub Logo',
+    },
+    description:
+      "HeroHub was created for superhero enthusiasts, whether they're newcomers to the comic book universe or long-time veterans. Here, every click reveals a treasure trove of detailed information about a myriad of legendary characters. From classic icons to newly emerged ones, all publishers have their place in this comprehensive catalog.",
+    links: [
+      {
+        label: 'View code',
+        url: 'https://github.com/LucasAnselmoSilva12345/hero-hub',
+        iconClassName: 'text-sky-700',
+      },
+      {
+        label: 'View website',
+        url: 'https://github.com/LucasAnselmoSilva12345/hero-hub',
+        iconClassName: 'text-sky-700',
+      },
+    ],
+  },
+  {
+    title: 'Social Pets',
+    technologies: ['React.JS', 'JavaScript'],
+    image: {
+      src: 'https://live.staticflickr.com/65535/53917583065_fb5c68ceb5_o.png',
+      alt: 'GitHub Logo',
+    },
+    description:
+      "Social Pets is an amazing website where pet owners can share pictures of their beloved pets with other like-minded individuals. With this website, you can create a profile and post photos of your furry friends for other users to see and comment on. It's a great way to connect with other pet owners and share stories about your pets.",
+    links: [
+      {
+        label: 'View code',
+        url: 'https://github.com/LucasAnselmoSilva12345/Social-Pets',
+        iconClassName: 'text-sky-700',
+      },
+      {
+        label: 'View website',
+        url: 'https://social-pets.pages.dev/',
+        iconClassName: 'text-sky-700',
+      },
+    ],
+  },
+  {
+    title: 'Aprenda JavaScript',
+    technologies: ['JavaScript', 'HTML', 'CSS'],
+    image: {
+      src: 'https://live.staticflickr.com/65535/53917583065_fb5c68ceb5_o.png',
+      alt: 'GitHub Logo',
+    },
+    description:
+      'Esse é um repositório de código JS feito para quem está començando na linguagem e gostaria de apreender algo específico na linguagem. E claro também é um repositório para quem já programa em JS e quer rever algum conceito da linguagem.',
+    links: [
+      {
+        label: 'View code',
+        url: 'https://github.com/LucasAnselmoSilva12345/aprenda_JavaScript',
+        iconClassName: 'text-sky-700',
+      },
+      {
+        label: 'View website',
+        url: 'https://github.com/LucasAnselmoSilva12345/aprenda_JavaScript',
+        iconClassName: 'text-sky-700',
+      },
+    ],
+  },
+  {
+    title: 'Projeto Site TI',
+    technologies: ['HTML', 'CSS'],
+    image: {
+      src: 'https://live.staticflickr.com/65535/53917583065_fb5c68ceb5_o.png',
+      alt: 'GitHub Logo',
+    },
+    description:
+      'This project is a website that presents about the 10 most used programming languages ​​in the world until December 2017.',
+    links: [
+      {
+        label: 'View code',
+        url: 'https://github.com/LucasAnselmoSilva12345/Projeto-Site-TI',
+        iconClassName: 'text-sky-700',
+      },
+      {
+        label: 'View website',
+        url: 'https://github.com/LucasAnselmoSilva12345/Projeto-Site-TI',
         iconClassName: 'text-sky-700',
       },
     ],
