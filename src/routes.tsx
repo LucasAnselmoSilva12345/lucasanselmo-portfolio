df8497763753deb1fbf212dcc70f28713f6993a7
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-11-17 15:36:14 -0300

    remove: About page
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home';
 import { Header } from './components/header';
 import { Footer } from './components/footer';
-import { About } from './pages/about';
 import { Project } from './pages/project';
 import { Articles } from './pages/articles';
 import { Networks } from './pages/networks';
@@ -14,7 +13,6 @@ export function RoutesApp() {
       <main>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
           <Route path="/projects" element={<Project />}></Route>
           <Route path="/articles" element={<Articles />}></Route>
           <Route path="/social-networks" element={<Networks />}></Route>
