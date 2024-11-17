71e110ce7ff254496aec0b30a431a25c92031449
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2024-11-17 16:02:54 -0300

    styles: adapt section margin
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/tech-stack.tsx
+++ src/components/tech-stack.tsx
@@ -2,8 +2,8 @@ import { TechnologiesImageGallery } from './technologies-image-gallery';
 
 export function TechStack() {
   return (
-    <div className="w-full mt-6 mb-2 md:max-w-3xl md:mx-auto">
-      <h2 className="mb-2 text-lg font-nunito font-bold">
+    <div className="w-full mt-4 mb-2 md:max-w-3xl md:mx-auto">
+      <h2 className="mb-4 text-lg font-nunito font-bold">
         The Technology Stack that Enhances My Workflow
       </h2>
       <TechnologiesImageGallery />
