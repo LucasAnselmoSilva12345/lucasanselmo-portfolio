117d6201efd2dce338b7ae5e8c5aef4afe377fa2
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2025-03-02 14:05:17 -0300

    feat: subtitle of article page
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -4,8 +4,14 @@ import { ReaderIcon } from '../components/network-icons/reader';
 
 export function Articles() {
   return (
-    <section className="w-full mt-6 mb-2 md:max-w-3xl md:mx-auto">
-      <h2 className="mb-4 text-lg font-nunito font-bold">Articles</h2>
+    <section className="w-full mt-6 mb-2 md:max-w-3xl md:mx-auto space-y-5">
+      <div>
+        <h2 className="text-lg font-nunito font-bold">Articles</h2>
+        <p className="text-sm font-nunito font-normal text-neutral-700">
+          Check out my scientific initiation and thesis articles, featuring
+          research and discoveries in development!
+        </p>
+      </div>
       <div className="space-y-5">
         <ExternalLinkButton.Root href="https://ocs.ifsp.edu.br/conict/xivconict/paper/view/9702">
           <ExternalLinkButton.Control>
