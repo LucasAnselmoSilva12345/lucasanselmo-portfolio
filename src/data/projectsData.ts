3eb3be9b2eaf19d68b118548d93490d206b74768
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2025-06-29 12:57:20 -0300

    feat: Set new projects data accordion environment develop
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,6 +1,6 @@
 import { IProjectsData } from '../utils/IProject';
 
-export const projectsData: IProjectsData[] = [
+export const projectProduction: IProjectsData[] = [
   {
     title: 'Sofá na Caixa',
     technologies: ['Shopify', 'JavaScript'],
@@ -65,6 +65,97 @@ export const projectsData: IProjectsData[] = [
       {
         label: 'Ver Site',
         url: 'https://beoliver.com.br',
+        iconClassName: 'text-sky-700',
+      },
+    ],
+  },
+];
+
+export const projectPersonal: IProjectsData[] = [
+  {
+    title: 'John Text List',
+    technologies: ['React.JS', 'TypeScript', 'ShadcnUI'],
+    image: {
+      src: 'https://live.staticflickr.com/65535/53917583065_fb5c68ceb5_o.png',
+      alt: 'GitHub Logo',
+    },
+    description:
+      'Este é um aplicativo web de lista de tarefas, responsável por ajudar você a organizar sua rotina de afazeres que precisam ser realizados ao longo do dia.',
+    links: [
+      {
+        label: 'Ver Código',
+        url: 'https://github.com/LucasAnselmoSilva12345/John-Task-List',
+        iconClassName: 'text-sky-700',
+      },
+      {
+        label: 'Ver Site',
+        url: 'https://john-task-list.pages.dev/',
+        iconClassName: 'text-sky-700',
+      },
+    ],
+  },
+  {
+    title: 'Rest Countries In The World',
+    technologies: ['React.JS', 'TypeScript', 'TailwindCSS'],
+    image: {
+      src: 'https://live.staticflickr.com/65535/53917583065_fb5c68ceb5_o.png',
+      alt: 'GitHub Logo',
+    },
+    description:
+      'O objetivo deste projeto é exibir todos os países do mundo por meio de cards individuais que mostram suas respectivas bandeiras, nomes, capitais, população total e continente.',
+    links: [
+      {
+        label: 'Ver Código',
+        url: 'https://github.com/LucasAnselmoSilva12345/restCountriesInTheWorld',
+        iconClassName: 'text-sky-700',
+      },
+      {
+        label: 'Ver Site',
+        url: 'https://restcountriesintheworld.pages.dev/',
+        iconClassName: 'text-sky-700',
+      },
+    ],
+  },
+  {
+    title: 'Hero Hub',
+    technologies: ['React.JS', 'TypeScript', 'TailwindCSS'],
+    image: {
+      src: 'https://live.staticflickr.com/65535/53917583065_fb5c68ceb5_o.png',
+      alt: 'GitHub Logo',
+    },
+    description:
+      'HeroHub foi criado para entusiastas de super-heróis, sejam eles novatos no universo dos quadrinhos ou veteranos de longa data. Aqui, a cada clique, você descobre um verdadeiro tesouro de informações detalhadas sobre uma infinidade de personagens lendários. Desde ícones clássicos até os mais recentes, todos os quadrinistas têm seu lugar neste catálogo completo.',
+    links: [
+      {
+        label: 'Ver Código',
+        url: 'https://github.com/LucasAnselmoSilva12345/hero-hub',
+        iconClassName: 'text-sky-700',
+      },
+      {
+        label: 'Ver Site',
+        url: 'https://github.com/LucasAnselmoSilva12345/hero-hub',
+        iconClassName: 'text-sky-700',
+      },
+    ],
+  },
+  {
+    title: 'Social Pets',
+    technologies: ['React.JS', 'JavaScript'],
+    image: {
+      src: 'https://live.staticflickr.com/65535/53917583065_fb5c68ceb5_o.png',
+      alt: 'GitHub Logo',
+    },
+    description:
+      'Social Pets é um site incrível onde donos de animais de estimação podem compartilhar fotos de seus queridos pets com outras pessoas que têm os mesmos interesses. Neste site, você pode criar um perfil e postar fotos dos seus amigos peludos para que outros usuários possam ver e comentar. É uma ótima maneira de se conectar com outros donos de animais e compartilhar histórias sobre seus pets.',
+    links: [
+      {
+        label: 'Ver Código',
+        url: 'https://github.com/LucasAnselmoSilva12345/Social-Pets',
+        iconClassName: 'text-sky-700',
+      },
+      {
+        label: 'Ver Site',
+        url: 'https://social-pets.pages.dev/',
         iconClassName: 'text-sky-700',
       },
     ],
